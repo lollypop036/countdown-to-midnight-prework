bd0aaf77d4dc447218321be5c19b8aa5372b5ae1
Lola Onanuga <slonanuga@gmail.com>
2018-09-23 12:07:34 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,20 +1,9 @@
-
 def countdown(n)
-<<<<<<< HEAD
   
   while n != 0
   
   puts "#{n} SECOND(S)!"  
   n -=1
-=======
-  counter = n
-  while counter != 0
-  puts "#{n} SECOND(S)!"  
-  counter -=1
-end
-return "HAPPY NEW YEAR!"
-  
->>>>>>> e43ddb595c7166a2e5bbbb25863ddabbf2d22474
 end
 return "HAPPY NEW YEAR!"
   
